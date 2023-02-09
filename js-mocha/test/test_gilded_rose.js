82bd4af68fcf0ee69df76951eaf3686fb66b5ffc
Xavi Ametller Serrat <xavi.ametller@gmail.com>
2023-02-09 23:41:43 +0100

    Add libraries for approval test and code coverage
    - approvals for approval testing
    - nyc for code coverage

--- js-mocha/test/test_gilded_rose.js
+++ js-mocha/test/test_gilded_rose.js
@@ -1,3 +1,6 @@
+require('approvals')
+  .mocha();
+
 var {expect} = require('chai');
 var {Shop, Item} = require('../src/gilded_rose.js');
 describe("Gilded Rose", function() {
@@ -8,4 +11,9 @@ describe("Gilded Rose", function() {
     expect(items[0].name).to.equal("fixme");
   });
 
+  it("should foo with verify", function() {
+    const gildedRose = new Shop([ new Item("foo", 0, 0) ]);
+    const items = gildedRose.updateQuality();
+    this.verifyAsJSON(items);
+  });   
 });
